5f4bdb9ac6fa27d00a068d0995457f6d3426391d
Valerieblade <153539281+Valerieblade@users.noreply.github.com>
2026-04-14 18:51:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import Auth from "./components/Auth";
-import { supabase } from "./lib/supabase";
+
 /* ─────────────────────────────────────────
    STYLES
 ───────────────────────────────────────── */
